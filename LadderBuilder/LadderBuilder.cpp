d14f74f82ac9d68e5dac5a56a6ef425fb7982068
Glencen <sylantiev21@gmail.com>
2024-06-23 23:42:28 +0300

    Добавлены комментарии для алгоритмов функций findLadders, findAllLadders, validateInput, exportResult и main

--- LadderBuilder/LadderBuilder.cpp
+++ LadderBuilder/LadderBuilder.cpp
@@ -6,13 +6,20 @@ using namespace std;
 
 // Функция для нахождения всех возможных лесенок, которые можно построить из указанного количества кубиков, с учетом количества кубиков в предыдущем уровне
 void findLadders(vector<int>& currentLadder, int remainingNumber, int previousNumber, vector<vector<int>>& ladders) {
+    // Если оставшееся количество кубиков равно 0
     if (remainingNumber == 0) {
+        // Сохранить текущую лесенку в общий контейнер
         ladders.push_back(currentLadder);
     }
+    // Иначе
     else {
+        // Для всех кубиков от 1 до наименьшего из двух чисел: оставшегося числа кубиков и количества кубиков, использованного в предыдущей итерации функции - 1
         for (int i = 1; i <= min(remainingNumber, previousNumber - 1); i++) {
+            // Записать в конец вектора, хранящего текущую лесенку, текущее используемое количество кубиков
             currentLadder.push_back(i);
+            // Рекурсивно вызвать функцию findLadders
             findLadders(currentLadder, remainingNumber - i, i, ladders);
+            // Удалить последний элемент вектора, хранящего текущую лесенку
             currentLadder.pop_back();
         }
     }
@@ -24,6 +31,7 @@ vector<vector<int>> findAllLadders(int N)
     vector<int> currentLadder;
     vector<vector<int>> ladders;
 
+    // Построить все лесенки из указанного количества кубиков, считая, что на предыдущем уровне должно быть кубиков на 1 больше, чем указанное количество
     findLadders(currentLadder, N, N + 1, ladders);
 
     return ladders;
@@ -32,26 +40,33 @@ vector<vector<int>> findAllLadders(int N)
 // Функция, проверяющая входные данные на правильность
 bool validateInput(string inputCheck, errors& error)
 {
+    // Если длина строки < 1
     if (inputCheck.length() < 1) {
+        // Считать, что входные данные отсутствуют
         error = noInputData;
         return false;
     }
+    // Иначе если длина строки >= 1 
     else if (inputCheck.length() >= 1) {
+        // Проверить строку на наличие символов, не являющихся цифрами
         bool inputValid = true;
         for (int i = 0; i < inputCheck.length(); i++) {
             if (!isdigit(inputCheck.at(i))) {
                 inputValid = false;
             }
         }
+        // Если строка содержит символы, не являющиеся цифрой 
         if (inputValid == false) {
+            // Считать, что введены недопустимые символы
             error = unacceptableSymbolsPresent;
             return false;
         }
     }
-
+    // Преобразовать строку в число
     int N = atoi(inputCheck.c_str());
-
+    // Если полученное число < 1 или > 100 
     if (N < 1 || N > 100) {
+        // Считать, что введено неверное число кубиков
         error = invalidCubeAmount;
         return false;
     }
@@ -63,7 +78,7 @@ bool validateInput(string inputCheck, errors& error)
 void exportResult(vector<vector<int>>& ladders, int N, string outputFileName)
 {
     ofstream output(outputFileName.c_str());
-
+    // Вывести все лесенки в виде суммы количества кубиков на каждом уровне и в виде их графического представления
     for (int i = 0; i < ladders.size(); i++) {
         output << N << " = ";
         for (int j = 0; j < ladders[i].size() - 1; j++) {
@@ -79,6 +94,7 @@ void exportResult(vector<vector<int>>& ladders, int N, string outputFileName)
         }
         output << endl << "-------------------------------------------------------------------------------------------" << endl << endl;
     }
+    // Вывести общее число найденных лесенок
     output << endl << ladders.size();
 }
 
@@ -88,7 +104,7 @@ int main(int argc, char* argv[]) {
     string outputFile = argv[2];
 
     ifstream input(inputFile.c_str());
-
+    // Если входной файл не открывается
     if (!input.is_open()) {
         cout << "Неверно указан файл для входных данных. Возможно указанного расположения не существует или нет прав на чтение" << endl;
         return 0;
@@ -98,9 +114,9 @@ int main(int argc, char* argv[]) {
     int N;
     errors error;
     vector<vector<int>> ladders;
-
+    // Считать входной файл
     getline(input, inputCheck);
-
+    // Если обнаружены ошибки во входных данных
     if (validateInput(inputCheck, error) == false) {
         if (error == noInputData) {
             cout << "Отсутствуют входные данные" << endl;
@@ -112,7 +128,9 @@ int main(int argc, char* argv[]) {
             cout << "Неверно указано количество кубиков. Количество должно быть в диапазоне от 1 до 100" << endl;
         }
     }
+    // Иначе
     else {
+        // Найти и вывести в выходной файл все лесенки, которые можно построить из данного количества кубиков
         N = atoi(inputCheck.c_str());
         ladders = findAllLadders(N);
         exportResult(ladders, N, outputFile);
