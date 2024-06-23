cf8cf1f9516afbcb817ea1b0c4a0f7f91f67bc58
Glencen <sylantiev21@gmail.com>
2024-06-23 22:22:46 +0300

    Реализованы функции findAllLadders, validateInput и exportResult

--- LadderBuilder/LadderBuilder.cpp
+++ LadderBuilder/LadderBuilder.cpp
@@ -18,23 +18,51 @@ void findLadders(vector<int>& currentLadder, int remainingNumber, int previousNu
     }
 }
 
-int main() {
-    setlocale(LC_ALL, "ru");
-    ifstream input("input.txt");
-    ofstream output("output.txt");
-
-    if (!input.is_open()) {
-        cout << "Неверно указан файл для входных данных. Возможно указанного расположения не существует или нет прав на чтение" << endl;
-        return 0;
-    }
-
-    string inputString;
-    getline(input, inputString);
-    int N = atoi(inputString.c_str());
-
+// Функция для нахождения всех возможных лесенок, которые можно построить из указанного количества кубиков
+vector<vector<int>> findAllLadders(int N)
+{
     vector<int> currentLadder;
     vector<vector<int>> ladders;
-    findLadders(currentLadder, N, N, ladders);
+
+    findLadders(currentLadder, N, N + 1, ladders);
+
+    return ladders;
+}
+
+// Функция, проверяющая входные данные на правильность
+bool validateInput(string inputCheck, errors& error)
+{
+    if (inputCheck.length() < 1) {
+        error = noInputData;
+        return false;
+    }
+    else if (inputCheck.length() >= 1) {
+        bool inputValid = true;
+        for (int i = 0; i < inputCheck.length(); i++) {
+            if (!isdigit(inputCheck.at(i))) {
+                inputValid = false;
+            }
+        }
+        if (inputValid == false) {
+            error = unacceptableSymbolsPresent;
+            return false;
+        }
+    }
+
+    int N = atoi(inputCheck.c_str());
+
+    if (N < 1 || N > 100) {
+        error = invalidCubeAmount;
+        return false;
+    }
+
+    return true;
+}
+
+// Функция, записывающая результат работы программы в указанный выходной файл
+void exportResult(vector<vector<int>>& ladders, int N, string outputFileName)
+{
+    ofstream output(outputFileName.c_str());
 
     for (int i = 0; i < ladders.size(); i++) {
         output << N << " = ";
@@ -51,14 +79,44 @@ int main() {
         }
         output << endl << "-------------------------------------------------------------------------------------------" << endl << endl;
     }
+    output << endl << ladders.size();
+}
 
-    output << N << " = " << N << endl << endl;
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "ru");
+    string inputFile = argv[1];
+    string outputFile = argv[2];
 
-    for (int i = 0; i < N; i++) {
-        output << "*";
+    ifstream input(inputFile.c_str());
+
+    if (!input.is_open()) {
+        cout << "Неверно указан файл для входных данных. Возможно указанного расположения не существует или нет прав на чтение" << endl;
+        return 0;
     }
-    output << endl << endl << "-------------------------------------------------------------------------------------------" << endl;
-    output << endl << ladders.size() + 1;
+
+    string inputCheck;
+    int N;
+    errors error;
+    vector<vector<int>> ladders;
+
+    getline(input, inputCheck);
+
+    if (validateInput(inputCheck, error) == false) {
+        if (error == noInputData) {
+            cout << "Отсутствуют входные данные" << endl;
+        }
+        else if (error == unacceptableSymbolsPresent) {
+            cout << "Введены недопустимые символы. Входные данные должны содержать только запись числа" << endl;
+        }
+        else if (error == invalidCubeAmount) {
+            cout << "Неверно указано количество кубиков. Количество должно быть в диапазоне от 1 до 100" << endl;
+        }
+    }
+    else {
+        N = atoi(inputCheck.c_str());
+        ladders = findAllLadders(N);
+        exportResult(ladders, N, outputFile);
+    }
 
     return 0;
 }
