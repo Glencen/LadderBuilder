fe2c3ad8e2fcdfa260826d4518d7214d97c5fabb
Glencen <sylantiev21@gmail.com>
2024-06-23 22:35:52 +0300

    Добавлены модульные тесты для функции findLadders

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -54,4 +54,167 @@ namespace Tests
 			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
 		}
 	};
+
+	TEST_CLASS(Test_findLadders)
+	{
+	public:
+
+		TEST_METHOD(oneOnPreviousLevelOneRemaining)
+		{
+			int remainingNumber = 1;
+			int previousNumber = 1;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 0;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(twoOnPreviousLevelOneRemaining)
+		{
+			int remainingNumber = 1;
+			int previousNumber = 2;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 1;
+			int actualLadderAmount = ladders.size();
+			vector<vector<int>> expectedLadders = { {1} };
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+			Assert::IsTrue(expectedLadders == ladders);
+		}
+
+		TEST_METHOD(eightOnPreviousLevelEightRemaining)
+		{
+			int remainingNumber = 8;
+			int previousNumber = 8;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 5;
+			int actualLadderAmount = ladders.size();
+			vector<vector<int>> expectedLadders = { {4,3,1},{5,2,1},{5,3},{6,2},{7,1} };
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+			Assert::IsTrue(expectedLadders == ladders);
+		}
+
+		TEST_METHOD(twentyOneOnPreviousLevelTwentyRemaining)
+		{
+			int remainingNumber = 20;
+			int previousNumber = 21;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 64;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(thirtyOnPreviousLevelTwentyRemaining)
+		{
+			int remainingNumber = 20;
+			int previousNumber = 30;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 64;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(fiveOnPreviousLevelSevenRemaining)
+		{
+			int remainingNumber = 7;
+			int previousNumber = 5;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 2;
+			int actualLadderAmount = ladders.size();
+			vector<vector<int>> expectedLadders = { {4,2,1},{4,3} };
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+			Assert::IsTrue(expectedLadders == ladders);
+		}
+
+		TEST_METHOD(fourOnPreviousLevelEightRemaining)
+		{
+			int remainingNumber = 8;
+			int previousNumber = 4;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 0;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(fiveOnPreviousLevelEightRemaining)
+		{
+			int remainingNumber = 8;
+			int previousNumber = 5;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 1;
+			int actualLadderAmount = ladders.size();
+			vector<vector<int>> expectedLadders = { {4,3,1} };
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+			Assert::IsTrue(expectedLadders == ladders);
+		}
+
+		TEST_METHOD(threeOnPreviousLevelSevenRemaining)
+		{
+			int remainingNumber = 7;
+			int previousNumber = 3;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 0;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(hunderedAndOneOnPreviousLevelHundredRemaining)
+		{
+			int remainingNumber = 100;
+			int previousNumber = 101;
+			vector<int> currentLadder;
+			vector<vector<int>> ladders;
+
+			findLadders(currentLadder, remainingNumber, previousNumber, ladders);
+
+			int expectedLadderAmount = 444793;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+	};
 }
