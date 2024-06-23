036f6f4c91642f9fad98f53255c6576554e26813
Glencen <sylantiev21@gmail.com>
2024-06-23 22:34:19 +0300

    Добавлены модульные тесты для функции findAllLadders

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -6,12 +6,52 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tests
 {
-	TEST_CLASS(Tests)
+	TEST_CLASS(Test_findAllLadders)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(minimumCubes)
 		{
+			int N = 1;
+			vector<vector<int>> ladders = findAllLadders(N);
+
+			int expectedLadderAmount = 1;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(maximumCubes)
+		{
+			int N = 100;
+			vector<vector<int>> ladders = findAllLadders(N);
+
+			int expectedLadderAmount = 444793;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(twelveCubes)
+		{
+			int N = 12;
+			vector<vector<int>> ladders = findAllLadders(N);
+
+			int expectedLadderAmount = 15;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
+		}
+
+		TEST_METHOD(twentyCubes)
+		{
+			int N = 20;
+			vector<vector<int>> ladders = findAllLadders(N);
+
+			int expectedLadderAmount = 64;
+			int actualLadderAmount = ladders.size();
+
+			Assert::AreEqual(expectedLadderAmount, actualLadderAmount);
 		}
 	};
 }
